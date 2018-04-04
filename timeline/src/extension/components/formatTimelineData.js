8f5c9d16961e5e72bab3ae2536fe0b13c56f3ec2
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-04-04 11:49:47 -0400

    fix timeline formatter

--- timeline/src/extension/components/formatTimelineData.js
+++ timeline/src/extension/components/formatTimelineData.js
@@ -10,9 +10,27 @@ export default function formatTimelineData(data) {
 
   for (let i = dataCache.length; i < data.length; i++) {
     if (i < data.length-1 && data[i].startTime === data[i+1].startTime && data[i].duration < data[i+1].duration){
-      let temp = data[i];
-      data[i] = data[i+1];
-      data[i+1] = temp;
+      let j = i;
+      let k = i;
+      
+      while (k < data.length-1 && data[k].startTime === data[k+1].startTime){
+        k++
+      }
+      for (let h = i; h < k; h++){
+        for (let l = h+1; l < k; l++){
+          if (data[l-1].duration < data[l].duration){
+            let temp = data[l-1];
+            data[l-1] = data[l];
+            data[l] = temp;
+          }
+        }
+      }
+      // while (j < data.length-1 && data[j].startTime === data[j+1].startTime && data[j].duration < data[j+1].duration){
+      //   let temp = data[j];
+      //   data[j] = data[j+1];
+      //   data[j+1] = temp;
+      //   j++;
+      // }
     }
 
     let datum = { x0: data[i].startTime/1000, x: (data[i].startTime + data[i].duration)/1000, name: data[i].name };
